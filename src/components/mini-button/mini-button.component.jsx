f1a8112b76a91fec1801f4b00d85b7a5c1ca65c2
Uye121 <yye90012@gmail.com>
2021-05-30 13:45:11 -0500

    Changed mini-button Link to a

--- src/components/mini-button/mini-button.component.jsx
+++ src/components/mini-button/mini-button.component.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import './mini-button.styles.scss';
 
 const MiniButton = ({ icon, url, text }) => (
-  <Link className='button' to={{ pathname: url}} target="_blank">
+  <a className='button' href={url} target="_blank" rel="noreferrer">
     <FontAwesomeIcon icon={icon} size='2x' color={'white'} />
     &nbsp;{text}
-  </Link>
+  </a>
 );
 
 export default MiniButton;
